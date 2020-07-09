18f6776f0f0b96c7938e1403b697b7d068e3e5a8
aneirine <anyashi2002@gmail.com>
2020-07-09 14:52:11 +0200

    Add test return method in movie resource

--- movie-info/src/main/java/com/test/movieinfo/resources/MovieResource.java
+++ movie-info/src/main/java/com/test/movieinfo/resources/MovieResource.java
@@ -1,5 +1,16 @@
 package com.test.movieinfo.resources;
 
+import com.test.movieinfo.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/movies")
 public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") long movieId){
+        return new Movie(movieId, "Test name");
+    }
 }
