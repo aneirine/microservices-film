3d11c617f990f4a30b6b73b9ed1d032c5f51919e
aneirine <anyashi2002@gmail.com>
2020-07-09 14:12:11 +0200

    First commit

---
+++ src/main/java/com/test/microservicefilm/MicroserviceFilmApplication.java
@@ -0,0 +1,13 @@
+package com.test.microservicefilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/microservicefilm/MicroserviceFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.microservicefilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
