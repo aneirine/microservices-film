99cd7e85102e53a04a11ea1a9d07d4e542afc6a2
aneirine <anyashi2002@gmail.com>
2020-07-09 14:31:37 +0200

    Add fields to catalog item

--- src/main/java/com/test/microservicefilm/models/CatalogItem.java
+++ src/main/java/com/test/microservicefilm/models/CatalogItem.java
@@ -1,4 +1,38 @@
 package com.test.microservicefilm.models;
 
 public class CatalogItem {
+
+    private String title;
+    private String description;
+    private double rating;
+
+    public CatalogItem(String title, String description, double rating) {
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
