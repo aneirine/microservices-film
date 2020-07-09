432fe6fcefc611ae4fe1fd1aaddb4d04df49a090
aneirine <anyashi2002@gmail.com>
2020-07-09 15:09:24 +0200

    Add new service movie-ratings

---
+++ movie-ratings/src/main/java/com/test/movieratings/MovieRatingsApplication.java
@@ -0,0 +1,13 @@
+package com.test.movieratings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingsApplication.class, args);
+	}
+
+}
