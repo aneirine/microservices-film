b460c83c41b14db4049d2f51bb2595cecf51e6b4
aneirine <anyashi2002@gmail.com>
2020-07-10 13:03:59 +0200

    Confid dependencies and names

--- movie-catalog/src/main/java/com/test/microservicefilm/MicroserviceFilmApplication.java
+++ movie-catalog/src/main/java/com/test/microservicefilm/MicroserviceFilmApplication.java
@@ -3,15 +3,13 @@ package com.test.microservicefilm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MicroserviceFilmApplication {
 
-
     @Bean
-    public WebClient.Builder getWebClientBuilder(){
+    public WebClient.Builder getWebClientBuilder() {
         return WebClient.builder();
     }
 
