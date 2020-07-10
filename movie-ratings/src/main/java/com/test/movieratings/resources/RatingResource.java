e30c7a7a709245d8d6056d90c6583c16c64bb828
aneirine <anyashi2002@gmail.com>
2020-07-10 10:34:08 +0200

    Add requested method, change ports

--- movie-ratings/src/main/java/com/test/movieratings/resources/RatingResource.java
+++ movie-ratings/src/main/java/com/test/movieratings/resources/RatingResource.java
@@ -1,5 +1,7 @@
 package com.test.movieratings.resources;
 
+import com.test.movieratings.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ratings")
 public class RatingResource {
 
-    @RequestMapping("")
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") long movieId) {
+        return new Rating(movieId, 1.2);
+    }
 }
