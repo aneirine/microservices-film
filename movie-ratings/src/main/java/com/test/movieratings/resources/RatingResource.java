cbce182be87b07c33223767621e1c6e391b44798
aneirine <anyashi2002@gmail.com>
2020-07-10 10:30:46 +0200

    Create rating model

---
+++ movie-ratings/src/main/java/com/test/movieratings/resources/RatingResource.java
@@ -0,0 +1,11 @@
+package com.test.movieratings.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingResource {
+
+    @RequestMapping("")
+}
