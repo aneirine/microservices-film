b9eb0ad97bb8bbf73d6537c7bb5e9f3a1de3d625
aneirine <anyashi2002@gmail.com>
2020-07-10 11:00:52 +0200

    Add connection between 2 microservices

--- movie-catalog/src/main/java/com/test/microservicefilm/resources/MovieCatalogResource.java
+++ movie-catalog/src/main/java/com/test/microservicefilm/resources/MovieCatalogResource.java
@@ -1,12 +1,16 @@
 package com.test.microservicefilm.resources;
 
 import com.test.microservicefilm.models.CatalogItem;
+import com.test.microservicefilm.models.Movie;
+import com.test.microservicefilm.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,8 +18,19 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Pirates", "New film about pirates", 4.6)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating(1, 5.6),
+                new Rating(2, 7.8)
         );
+
+        return ratings.stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "test desc", rating.getRating());
+                }
+        ).collect(Collectors.toList());
+
     }
 }
