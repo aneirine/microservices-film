3be482ed94f3323d44e3c482af8b7b4a3f0b7234
aneirine <anyashi2002@gmail.com>
2020-07-10 13:17:23 +0200

    Add and implement lombok

--- movie-catalog/src/main/java/com/test/microservicefilm/MicroserviceFilmApplication.java
+++ movie-catalog/src/main/java/com/test/microservicefilm/MicroserviceFilmApplication.java
@@ -2,6 +2,7 @@ package com.test.microservicefilm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,6 +12,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class MicroserviceFilmApplication {
 
     @Bean
+    @LoadBalanced
     public WebClient.Builder getWebClientBuilder() {
         return WebClient.builder();
     }
--- movie-catalog/src/main/java/com/test/microservicefilm/models/Movie.java
+++ movie-catalog/src/main/java/com/test/microservicefilm/models/Movie.java
@@ -1,31 +1,14 @@
 package com.test.microservicefilm.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Movie {
 
     private long id;
     private String name;
 
-    public Movie() {
-    }
 
-    public Movie(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
