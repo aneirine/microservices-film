4a3d66a4ef5e41807f84b0d9c78f28730582ad40
aneirine <anyashi2002@gmail.com>
2020-07-10 12:39:52 +0200

    Configure eureka

--- discovery-server/src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
@@ -1,9 +1,12 @@
 package io.javabrains.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
