9d05833babed1ad0f0110b9ea3d2ef0ba3500feb
aneirine <anyashi2002@gmail.com>
2020-07-13 10:51:30 +0200

    Add right dependencies

--- movie-catalog/src/main/java/com/test/microservicefilm/config/BeanConfiguration.java
+++ movie-catalog/src/main/java/com/test/microservicefilm/config/BeanConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class BeanConfiguration {
@@ -15,9 +14,5 @@ public class BeanConfiguration {
         return new RestTemplate();
     }
 
-    @Bean
-    @LoadBalanced
-    public WebClient.Builder getWebClientBuilder() {
-        return WebClient.builder();
-    }
+
 }
