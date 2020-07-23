8be4bcf498770449ac0a4fb7081d7aada9192d9c
Aneirine <anyashi2002@gmail.com>
2020-07-23 21:57:45 +0300

    Add hystrix dependency

--- movie-info/src/main/java/com/test/movieinfo/services/MovieService.java
+++ movie-info/src/main/java/com/test/movieinfo/services/MovieService.java
@@ -1,11 +1,14 @@
 package com.test.movieinfo.services;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.test.movieinfo.models.Movie;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class MovieService {
+
+
 
     public Movie getMovieInfo(long movieId) {
         return new Movie(movieId, "Test title", "Test descrpition");
