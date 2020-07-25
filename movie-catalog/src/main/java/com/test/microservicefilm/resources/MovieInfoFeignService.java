3a85a48d3b5f43e1c894511a9079fe652ce55824
Aneirine <anyashi2002@gmail.com>
2020-07-25 21:15:50 +0300

    Add configuration for ports of movie info

--- movie-catalog/src/main/java/com/test/microservicefilm/resources/MovieInfoFeignService.java
+++ movie-catalog/src/main/java/com/test/microservicefilm/resources/MovieInfoFeignService.java
@@ -1,6 +1,7 @@
 package com.test.microservicefilm.resources;
 
 import com.test.microservicefilm.models.Movie;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.ws.rs.core.MediaType;
 
 @FeignClient(name = "movie-info-service")
+@RibbonClient(name = "movie-info-service")
 @RequestMapping("/movies")
 public interface MovieInfoFeignService {
 
