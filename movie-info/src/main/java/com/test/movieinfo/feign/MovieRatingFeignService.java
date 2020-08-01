0695b77552540082671b746c706f5ee7ce945f4b
Aneirine <anyashi2002@gmail.com>
2020-08-01 19:02:27 +0300

    Add zuul api path in feign client

--- movie-info/src/main/java/com/test/movieinfo/feign/MovieRatingFeignService.java
+++ movie-info/src/main/java/com/test/movieinfo/feign/MovieRatingFeignService.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "movie-ratings")
-@RibbonClient(name = "movie-ratings")
-@RequestMapping("/ratings")
+@FeignClient(name = "zuul-api-gateway")
+//@RibbonClient(name = "movie-ratings")
+@RequestMapping("/movie-ratings/ratings")
 public interface MovieRatingFeignService {
 
     @GetMapping("/{movieId}")
