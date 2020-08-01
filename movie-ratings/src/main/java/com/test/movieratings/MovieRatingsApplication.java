2fe54238b9b229e4dbb3e40af56d04bc9abc9f44
Aneirine <anyashi2002@gmail.com>
2020-08-01 22:33:44 +0300

    Config application

--- movie-ratings/src/main/java/com/test/movieratings/MovieRatingsApplication.java
+++ movie-ratings/src/main/java/com/test/movieratings/MovieRatingsApplication.java
@@ -2,6 +2,7 @@ package com.test.movieratings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
 public class MovieRatingsApplication {
 
 	public static void main(String[] args) {
